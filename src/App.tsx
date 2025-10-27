80697283b5cc35e29909d26c9c9e8d9ba6dbb958
Carlos <cclugo@gmail.com>
2025-10-27 14:53:52 +0900

    feat: CSS adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -56,31 +56,31 @@ function App() {
         {error && <p className="text-red-500">{error}</p>}
         {isAddingSession ? (
           <div>
-            <h2 className="text-2xl font-bold mb-4">New Session</h2>
-            <div className="flex flex-col space-y-4">
-              <button onClick={() => handleSessionTypeClick('legs')} className="bg-gray-800 text-white font-bold py-4 px-4 rounded-lg text-xl">
+            <h2 className="text-2xl font-bold mb-1">New Session</h2>
+            <div className="flex flex-col space-y-2">
+              <button onClick={() => handleSessionTypeClick('legs')} className="bg-gray-800 text-white font-bold py-2 px-4 rounded-lg text-xl">
                 Legs
               </button>
-              <button onClick={() => handleSessionTypeClick('back')} className="bg-gray-800 text-white font-bold py-4 px-4 rounded-lg text-xl">
+              <button onClick={() => handleSessionTypeClick('back')} className="bg-gray-800 text-white font-bold py-2 px-4 rounded-lg text-xl">
                 Back
               </button>
-              <button onClick={() => handleSessionTypeClick('chest')} className="bg-gray-800 text-white font-bold py-4 px-4 rounded-lg text-xl">
+              <button onClick={() => handleSessionTypeClick('chest')} className="bg-gray-800 text-white font-bold py-2 px-4 rounded-lg text-xl">
                 Chest
               </button>
             </div>
           </div>
         ) : (
           <div>
-            <h2 className="text-2xl font-bold mb-4">Latest Session</h2>
+            <h2 className="text-2xl font-bold mb-1">Latest Session</h2>
             {latestSession ? (
-              <div className="bg-gray-800 p-4 rounded-lg">
-                <p className="text-xl font-bold">{latestSession.session_type}</p>
-                <p className="text-gray-400">{new Date(latestSession.created_at).toLocaleDateString()}</p>
+              <div className="bg-gray-800 p-2 rounded-lg">
+                <p className="text-2xl font-bold">{latestSession.session_type}</p>
+                <p className="text-xl">{new Date(latestSession.created_at).toLocaleDateString()}</p>
               </div>
             ) : (
               <p>No sessions yet.</p>
             )}
-            <button onClick={handleRegisterClick} className="mt-8 bg-blue-600 text-white font-bold py-4 px-4 rounded-full text-xl">
+            <button onClick={handleRegisterClick} className="mt-2 bg-blue-600 text-white font-bold py-2 px-4 rounded-full text-xl">
               Register New Session
             </button>
           </div>
